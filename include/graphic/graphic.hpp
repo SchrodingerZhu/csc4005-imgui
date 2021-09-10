91f8aecaff35498240b90c473ac5c3d062b4bd0a
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-09-11 00:52:07 +0800

    add ignore
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

---
+++ include/graphic/graphic.hpp
@@ -0,0 +1,8 @@
+//
+// Created by schrodinger on 9/9/21.
+//
+
+#ifndef CSC4005_IMGUI_GRAPHIC_HPP
+#define CSC4005_IMGUI_GRAPHIC_HPP
+
+#endif //CSC4005_IMGUI_GRAPHIC_HPP
---
+++ src/graphic.cpp
@@ -0,0 +1,4 @@
+//
+// Created by schrodinger on 9/9/21.
+//
+
